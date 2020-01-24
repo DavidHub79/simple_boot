643fd0ab83b34e0fca96cfed921c1adaecc521a0
unknown <demartinica@hotmail>
2020-01-24 14:28:19 +0100

    refactoring of simpleboot WS

--- src/main/java/com/formation/ctrl/CalculatorController.java
+++ src/main/java/com/formation/ctrl/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.formation.ctrl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formation.service.Calculator;
@@ -10,7 +12,8 @@ public class CalculatorController {
 	@Autowired
 	private Calculator calculator;
 	
-	public  String  sum(Integer a, Integer b) {
+	@RequestMapping("/sum")
+	public  String  sum(@RequestParam("a")Integer a, @RequestParam("b")Integer b) {
 		return String.valueOf(calculator.sum(a, b));
 	}
 
